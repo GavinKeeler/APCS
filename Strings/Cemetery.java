14e1c9b3d4ef9d8c51aa7004769b25de9e31d222
GavinKeeler <gavinakeeler@gmail.com>
2025-11-12 11:11:43 -0500

    Create Cemetery.java

---
+++ Strings/Cemetery.java
@@ -0,0 +1,205 @@
+// Name:
+// Date:
+import java.util.*;
+import java.io.*;
+import java.text.DecimalFormat;
+
+public class Cemetery
+{
+   public static void main (String [] args)
+   {
+      File file = new File("cemetery.txt");
+      //File file = new File("cemetery.txt");
+      int numEntries = countEntries(file);
+      Person[] cemetery = readIntoArray(file, numEntries); 
+      //uncomment to see if you have read the data properly
+      //for (int i = 0; i < cemetery.length; i++) 
+         //System.out.println(cemetery[i]);
+         
+      int min = locateMinAgePerson(cemetery);
+      int max = locateMaxAgePerson(cemetery); 
+      System.out.println("\nIn the St. Mary Magdelene Old Fish Cemetery: ");
+      System.out.println("Name of youngest person: " + cemetery[min].getName());
+      System.out.println("Age of youngest person: " + cemetery[min].getAge());    
+      System.out.println("Name of oldest person: " + cemetery[max].getName());
+      System.out.println("Age of oldest person: " + cemetery[max].getAge()); 
+      //you may create other testing cases here
+     
+          
+   }
+   
+   
+   /* Counts and returns the number of entries in File f. 
+      Returns 0 if the File f is not valid.
+      Uses a try-catch block.   
+      @param f -- the file object
+   */
+   public static int countEntries(File f)
+   {
+      int i =0;
+      Scanner sc=null;
+      try{
+        sc = new Scanner(f);  
+      }catch(IOException e){
+          return 0;
+      }
+      while(sc.hasNextLine()){
+          sc.nextLine();
+          i++;
+      }
+      return i;
+   }
+
+   /* Reads the data from file f (you may assume each line has same allignment).
+      Fills the array with Person objects. If File f is not valid return null.
+      @param f -- the file object 
+      @param num -- the number of lines in the File f  
+   */
+   public static Person[] readIntoArray (File f, int num)
+   {
+   Scanner sc =null;
+      try{
+        sc = new Scanner(f);  
+      }catch(IOException e){
+          return null;
+      }
+      Person[] arr= new Person[num];
+      for(int i =0; i<num; i++){
+          arr[i]=makeObjects(sc.nextLine());
+      }
+      return arr;
+   }
+   
+   
+   /* A helper method that instantiates one Person object.
+      @param entry -- one line of the input file.
+      This method is made public for gradeit testing purposes.
+      This method should not be used in any other class!!!
+   */
+   public static Person makeObjects(String entry)
+   {
+       String name="",date="",age="";
+       
+        for(int i =0; i<entry.length();i++)
+            if(Character.isDigit(entry.charAt(i))||entry.substring(i,i+2).equals("xx")){
+                name=entry.substring(0,i).trim();
+                date = entry.substring(i,i+11);
+                int j =0;
+                String digits="1234567890.wd";
+                while(digits.contains(entry.substring(i+12+j,i+13+j))){
+                age +=entry.charAt(i+12+j);
+                j++;}
+                age=age.trim();
+                break;
+            }
+        return(new Person(name,date,age));
+   }  
+   
+   /* Finds and returns the location (the index) of the Person
+      who is the youngest. (if the array is empty it returns -1)
+      If there is a tie the lowest index is returned.
+      @param arr -- an array of Person objects.
+   */
+   public static int locateMinAgePerson(Person[] arr)
+   {
+    if (arr.length==0)
+        return -1;
+    int minIndex=0;
+    for(int i=0;i<arr.length;i++)
+        if(arr[i].getAge()<arr[minIndex].getAge())
+            minIndex=i;
+    return minIndex;
+   }   
+   
+   /* Finds and returns the location (the index) of the Person
+      who is the oldest. (if the array is empty it returns -1)
+      If there is a tie the lowest index is returned.
+      @param arr -- an array of Person objects.
+   */
+   public static int locateMaxAgePerson(Person[] arr)
+   {
+   if (arr.length==0)
+        return -1;
+    int maxIndex=0;
+    for(int i=0;i<arr.length;i++)
+        if(arr[i].getAge()>arr[maxIndex].getAge())
+            maxIndex=i;
+    return maxIndex;
+   }        
+} 
+
+class Person
+{
+   //constant that can be used for formatting purposes
+   private static final DecimalFormat df = new DecimalFormat("0.0000");
+   /* private fields */
+   String myName;
+   String myDate;
+   String myAge;
+   
+      
+   /* a three-arg constructor  
+    @param name, burialDate may have leading or trailing spaces
+    It creates a valid Person object in which each field has the leading and trailing
+    spaces eliminated*/
+   public Person(String name, String burialDate, String age)
+   {
+   myName=name;
+   myDate=burialDate;
+   myAge=age;
+   }
+   /* any necessary accessor methods (at least "double getAge()" and "String getName()" )
+   make sure your get and/or set methods use the same data type as the field  */
+   public String getName(){
+       return myName;
+   }
+   public String getDate(){
+       return myDate;
+   }
+   public double getAge(){
+       return calculateAge(myAge);
+   }
+   public void setName(String n){
+        myName=n;
+   }
+   public void setDate(String d){
+        myDate=d;
+   }
+   public void setAge(String a){
+        myAge=a;
+   }
+   
+   /*handles the inconsistencies regarding age
+     @param a = a string containing an age from file. Ex: "12", "12w", "12d"
+     returns the age transformed into year with 4 decimals rounding
+   */
+   public double calculateAge(String a)
+   {
+   if(a.contains("d"))
+       return Double.parseDouble(df.format(Double.parseDouble(a.substring(0,a.indexOf("d")))/365));
+    if(a.contains("w"))
+       return Double.parseDouble(df.format(Double.parseDouble(a.substring(0,a.indexOf("w")))/52));
+    return Double.parseDouble(df.format(Double.parseDouble(a)));
+   }
+}
+
+/******************************************
+
+ John William ALLARDYCE, 17 Mar 1844, 2.9
+ Frederic Alex. ALLARDYCE, 21 Apr 1844, 0.17
+ Philip AMIS, 03 Aug 1848, 1.0
+ Thomas ANDERSON, 06 Jul 1845, 27.0
+ Edward ANGEL, 20 Nov 1842, 22.0
+ Lucy Ann COLEBACK, 23 Jul 1843, 0.2685
+ Thomas William COLLEY, 08 Aug 1833, 0.011
+ Joseph COLLIER, 03 Apr 1831, 58.0
+ 
+ In the St. Mary Magdelene Old Fish Cemetery --> 
+ Name of youngest person: Thomas William COLLEY
+ Age of youngest person: 0.011
+ Name of oldest person: Joseph COLLIER
+ Age of oldest person: 58.0
+ 
+ **************************************/
+
+
